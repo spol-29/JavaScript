298bdd5f374492cc8b993882d71503cc99b3d6ba
spol-29 <polineni.sriram9@gmail.com>
2023-02-25 16:39:29 -0600

    edit index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
-// take the operand input
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
 let result;
 
-// using if...else if... else
 if (operator == '+') {
     result = number1 + number2;
 }
